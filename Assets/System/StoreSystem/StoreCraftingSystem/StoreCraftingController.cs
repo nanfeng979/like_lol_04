000d3efd038a9f70a1c6e6f4f386ff11524f8223
nanfeng979 <306338401@qq.com>
2025-10-07 00:54:32 +0800

    StoreCategorySystem：初步实现多层渲染

--- Assets/System/StoreSystem/StoreCraftingSystem/StoreCraftingController.cs
+++ Assets/System/StoreSystem/StoreCraftingSystem/StoreCraftingController.cs
@@ -40,10 +40,36 @@ namespace LikeLoL04
                     }
                 }
                 view.UpdateParents(parents);
+                // 第一层 Children
+                var layer1Children = new List<StoreItemModel>();
+                var layer2Children = new List<StoreItemModel>();
+                foreach (var childName in craftingModel.Children)
+                {
+                    StoreItemModel childItem = StoreSystemController.Instance.GetItemByName(childName);
+                    if (childItem != null)
+                    {
+                        layer1Children.Add(childItem);
+                        // 查找该 child 的 crafting 数据，收集第二层（即孙辈）
+                        var childCraft = m_craftingTable.Find(c => c.Name == childName);
+                        if (childCraft != null && childCraft.Children != null)
+                        {
+                            foreach (var grand in childCraft.Children)
+                            {
+                                var grandItem = StoreSystemController.Instance.GetItemByName(grand);
+                                if (grandItem != null)
+                                {
+                                    layer2Children.Add(grandItem);
+                                }
+                            }
+                        }
+                    }
+                }
+                view.MultiLayerUpdateChildren(layer1Children, layer2Children);
             }
             else
             {
                 view.UpdateParents(null);
+                view.MultiLayerUpdateChildren(null, null);
             }
         }
     }
--- Assets/System/StoreSystem/StoreCraftingSystem/StoreCraftingView.cs
+++ Assets/System/StoreSystem/StoreCraftingSystem/StoreCraftingView.cs
@@ -15,6 +15,15 @@ namespace LikeLoL04
         [SerializeField]
         public RectTransform BeSelectItemParents;
 
+        [SerializeField]
+        private RectTransform m_ChildRow1;
+
+        [SerializeField]
+        private RectTransform m_ChildRow2;
+
+        [SerializeField]
+        private RectTransform m_ChildRow3;
+
         void Start()
         {
             foreach (RectTransform child in BeSelectItemParents)
@@ -52,6 +61,96 @@ namespace LikeLoL04
                 }
 
             }
+        }
+
+        public void MultiLayerUpdateChildren(List<StoreItemModel> layer1, List<StoreItemModel> layer2)
+        {
+            // 先全部隐藏子物体
+            for (int i = 0; i < m_ChildRow1.childCount; i++)
+                m_ChildRow1.GetChild(i).gameObject.SetActive(false);
+            for (int i = 0; i < m_ChildRow2.childCount; i++)
+                m_ChildRow2.GetChild(i).gameObject.SetActive(false);
+            for (int i = 0; i < m_ChildRow3.childCount; i++)
+                m_ChildRow3.GetChild(i).gameObject.SetActive(false);
+
+            // 默认隐藏所有行
+            m_ChildRow1.gameObject.SetActive(false);
+            m_ChildRow2.gameObject.SetActive(false);
+            m_ChildRow3.gameObject.SetActive(false);
+
+            if ((layer1 == null || layer1.Count == 0) && (layer2 == null || layer2.Count == 0))
+            {
+                return;
+            }
+
+            int row1Count = m_ChildRow1.childCount;
+            int row2Count = m_ChildRow2.childCount;
+            int row3Count = m_ChildRow3.childCount;
+
+            bool row1Active = false, row2Active = false, row3Active = false;
+
+            int total = (layer1?.Count ?? 0) + (layer2?.Count ?? 0);
+            for (int i = 0; i < total; i++)
+            {
+                RectTransform childItem = null;
+                if (i < row1Count)
+                {
+                    childItem = m_ChildRow1.GetChild(i).GetComponent<RectTransform>();
+                    row1Active = true;
+                }
+                else if (i < row1Count + row2Count)
+                {
+                    childItem = m_ChildRow2.GetChild(i - row1Count).GetComponent<RectTransform>();
+                    row2Active = true;
+                }
+                else if (i < row1Count + row2Count + row3Count)
+                {
+                    childItem = m_ChildRow3.GetChild(i - row1Count - row2Count).GetComponent<RectTransform>();
+                    row3Active = true;
+                }
+                else
+                {
+                    break;
+                }
+
+                childItem.gameObject.SetActive(true);
+                StoreItemView childItemView = childItem.GetComponent<StoreItemView>();
+                int index = i;
+                childItemView.OnLeftClickAction = () =>
+                {
+                    StoreItemModel target;
+                    if (index < (layer1?.Count ?? 0))
+                    {
+                        target = layer1[index];
+                    }
+                    else
+                    {
+                        int offset = index - (layer1?.Count ?? 0);
+                        target = layer2[offset];
+                    }
+                    StoreCraftingController.Instance.SetBeSelectStoreItem(target);
+                };
+                if (index < (layer1?.Count ?? 0))
+                {
+                    childItemView.SetItem(layer1[index]);
+                }
+                else
+                {
+                    int offset = index - (layer1?.Count ?? 0);
+                    childItemView.SetItem(layer2[offset]);
+                }
+            }
+
+            // 只激活有内容的行
+            if (row1Active) m_ChildRow1.gameObject.SetActive(true);
+            if (row2Active) m_ChildRow2.gameObject.SetActive(true);
+            if (row3Active) m_ChildRow3.gameObject.SetActive(true);
+        }
+
+        // 兼容旧调用（只传一层）
+        public void UpdateChildren(List<StoreItemModel> onlyLayer1)
+        {
+            MultiLayerUpdateChildren(onlyLayer1, null);
         }
     }
 }
